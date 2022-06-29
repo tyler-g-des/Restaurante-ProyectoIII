47a5e480d1cf2e83866df46520d5a9c98fd96384
tyler-g-des <tylercherield@gmail.com>
2022-06-29 14:01:10 -0400

    cree la entidad para las mesas

---
+++ backend/roya/src/main/java/com/restaurante/roya/models/Tablets.java
@@ -0,0 +1,42 @@
+package com.restaurante.roya.models;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tablets")
+public class Tablets{
+
+    @Column(unique = true, nullable = false)
+    private Long id;
+
+    @Column(nullable = false)
+    private String status;
+
+    private String description;
+
+    public long getId(){
+        return id;
+    }
+
+    public void setId(Long id){
+        this.id = id;
+    }
+
+    public String getStatus(){
+        return status;
+    }
+
+    public void setStatus(String status){
+        this.status = status;
+    }
+
+    public String getDescription(){
+        return description;
+    }
+
+    public void setDescription(String description){
+        this.description = description;
+    }
+}
