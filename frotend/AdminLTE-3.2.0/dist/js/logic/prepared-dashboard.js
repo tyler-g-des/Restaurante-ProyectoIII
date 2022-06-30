81981f461f9ab20b1ff07edb1d3b2e0fe6f882b9
tyler-g-des <tylercherield@gmail.com>
2022-06-30 13:49:46 -0400

    empece a agregar logica para las mesas

--- frotend/AdminLTE-3.2.0/dist/js/logic/prepared-dashboard.js
+++ frotend/AdminLTE-3.2.0/dist/js/logic/prepared-dashboard.js
@@ -2,3 +2,22 @@ let name = window.localStorage.getItem('name');
 let age = window.localStorage.getItem('rol');
 
 console.log(name, age); 
+
+let mesa =  document.querySelector('#mesa1');
+
+
+mesa.addEventListener('load', event => {
+    console.log('kdkkk');
+    event.preventDefault();
+    console.log('sadda');
+    mesaEstados();  
+});
+
+
+console.log('fadsfds');
+
+const mesaEstados = async () => {
+    const response = await axios.get('http://localhost:8080/tablets',{
+   });
+   console.log(response);
+}
