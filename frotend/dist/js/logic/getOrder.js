649cd44e9a9ee1fca596d3e814eb52738a39a418
tyler-g-des <tylercherield@gmail.com>
2022-07-17 14:33:41 -0400

    logica para trabajar ordenes desde la pagina de ordenes

--- frotend/dist/js/logic/getOrder.js
+++ frotend/dist/js/logic/getOrder.js
@@ -1,7 +1,9 @@
 // Acciones 
 const boton = document.querySelector("#iniciarPedido");
 const botonTomarOrdenes = document.querySelector("#tomarOrdenes");
-
+const botonVolver = document.querySelector("#volverOrdenes");
+let footer;
+let incremento=0;
 // Fecha y hora
 let fecha = new Date();
 let hora = fecha.getHours() + ':' + fecha.getMinutes() + ':' + fecha.getSeconds();
@@ -10,11 +12,8 @@ document.getElementById("fecha").value =  fecha.toJSON().slice(0,10);
 
 //Obtener cantidad de personas
 boton.addEventListener('click', event => {
-    let numeroPersonas =  document.getElementById("numeroPersona");
-    console.log(numeroPersonas.value);
-  
+   let numeroPersonas = document.getElementById("numeroPersona");
 });
-
 
 botonTomarOrdenes.addEventListener('click', event => {
     let nombre = document.querySelector("#nombreCliente");
@@ -25,6 +24,8 @@ botonTomarOrdenes.addEventListener('click', event => {
    }
    else{
 
+       let numeroPersonas = document.getElementById("numeroPersona");
+
         //Formulario para la orden 
         let plato = document.querySelector("#plato");
         let bebida = document.querySelector("#bebida");
@@ -32,12 +33,47 @@ botonTomarOrdenes.addEventListener('click', event => {
         let fecha = document.querySelector("#fecha");
         let fechaModificada = fecha.value + " "  + hora;
 
-        let pedido = [nombre.value, plato.value, bebida.value, postre.value , fechaModificada];
-        localStorage.setItem("pedido", JSON.stringify(pedido));
+        //Guardar Orden Local Storage
+        let pedido = []; 
+        pedido[incremento] = [nombre.value, plato.value, bebida.value, postre.value , fechaModificada];
+        sessionStorage.setItem("pedido"+incremento, JSON.stringify(pedido[incremento]));
 
         document.getElementById("nombreCliente").value =  "";
+        alert("siguiente orden");
 
-        alert("siguiente orden")
+        //Cambiar comportamiento
+        footer = document.querySelector("#ordenFooter");
+        //footer = footer.textContent + " " + numeroPersonas.value;
+        numeroPersonas.value --;
+        incremento ++;
+        footer.textContent = "Ordenes Restantes: " + numeroPersonas.value;
+
+        //Logica cancelar Ordenes
+        botonVolver.textContent = "Cancelar Orden"
+        
+
+        // Al completar los pedidos guardar en base de Datos y regresar a la pagina de tomar ordenes
+        if(numeroPersonas.value == 0 || numeroPersonas ==  "0"){
+          alert("Ordenes Tomadas");
+          //AQUI GUARDAR DATOS DE LA ORDEN
+          prepararPaginaOrdenes();
+        }
    }
 
 });
+
+
+botonVolver.addEventListener('click', event => {
+  if(botonVolver.textContent == "Cancelar Orden")
+  {
+    prepararPaginaOrdenes();
+  }
+});
+
+
+const prepararPaginaOrdenes = () => {
+  sessionStorage.clear();
+  window.location.replace("tomarOrdenes.html"); 
+  botonVolver.textContent = "Volver"
+  incremento = 0;
+ }
